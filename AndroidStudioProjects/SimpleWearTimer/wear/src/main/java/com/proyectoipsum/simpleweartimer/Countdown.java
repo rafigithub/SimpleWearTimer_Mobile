2d500846086c759d214645fa60cc0abf6359645f
Rafi <rsahakian@gmail.com>
2016-05-16 19:39:55 -0300

    Simple Wear Timer source code

---
+++ AndroidStudioProjects/SimpleWearTimer/wear/src/main/java/com/proyectoipsum/simpleweartimer/Countdown.java
@@ -0,0 +1,66 @@
+package com.proyectoipsum.simpleweartimer;
+
+import android.content.Context;
+import android.os.CountDownTimer;
+import android.os.Vibrator;
+import android.widget.TextView;
+
+/**
+ * Created by Rafi on 26/04/2016.
+ */
+public class Countdown implements Timer {
+
+    private MainActivity mainActivity;
+    private CountDownTimer testTimer;
+    private String startingTime;
+
+
+    public Countdown(final Context context, String startingTime){
+
+        mainActivity = (MainActivity) context;
+        this.startingTime = startingTime;
+        //setTimerView(timerView);
+
+    }
+
+
+    public void setUpTimer(final TextView timerDisplay){
+
+        final String timeString = timerDisplay.getText().toString();
+        long millisRemaining = MilliConversions.stringToMilli(timeString);
+
+        testTimer  = new CountDownTimer(millisRemaining, 400) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                String timeRemaining = MilliConversions.milliToString(millisUntilFinished);
+                timerDisplay.setText(timeRemaining);
+            }
+
+            @Override
+            public void onFinish() {
+
+                Vibrator v = (Vibrator) mainActivity.getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(300);
+            }
+        };
+    }
+
+    public void startTimer(){
+
+        testTimer.start();
+    }
+
+    public void cancelTimer(){
+
+        if(testTimer!=null){
+
+            testTimer.cancel();
+        }
+    }
+
+    public String getStartingTime(){
+
+        return startingTime;
+    }
+}
